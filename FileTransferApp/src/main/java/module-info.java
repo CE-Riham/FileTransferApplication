fd4763f1e221e8c62e06ee754a1ec4b818e996c0
Amjad <amjad.kayed@hotmail.com>
2023-08-05 14:21:41 +0300

    init project

--- FileTransferApp/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.filetransferapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.filetransferapp to javafx.fxml;
-    exports com.example.filetransferapp;
-}
